dcc22635a0263af0a294055f6799b8e87f3e1fc3
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-04-12 14:46:20 +0200

    image joins

--- src/main/java/pl/piotrdawidziuk/Controller/JoinImageController.java
+++ src/main/java/pl/piotrdawidziuk/Controller/JoinImageController.java
@@ -1,8 +1,15 @@
 package pl.piotrdawidziuk.Controller;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+
+import javax.imageio.ImageIO;
 
 import org.apache.commons.io.IOUtils;
 import org.springframework.http.MediaType;
@@ -14,10 +21,36 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class JoinImageController {
 	
 	@GetMapping(value = "/get-joined", produces = MediaType.IMAGE_JPEG_VALUE)
-    public @ResponseBody byte[] getJoinedImage() throws IOException {
-    	InputStream in = new URL("https://vignette.wikia.nocookie.net/uncyclopedia/images/b/be/Cat.JPG").openStream();  
-        return IOUtils.toByteArray(in);
+	public @ResponseBody byte[] getJoinedImage() throws IOException {
+		
+		BufferedImage img1 = ImageIO.read(new URL("https://vignette.wikia.nocookie.net/uncyclopedia/images/b/be/Cat.JPG"));
+		BufferedImage img2 = ImageIO.read(new URL("https://vignette.wikia.nocookie.net/uncyclopedia/images/b/be/Cat.JPG"));
+		BufferedImage joinedImg = joinBufferedImage(img1, img2);
+		
+		
+		ByteArrayOutputStream os = new ByteArrayOutputStream();
+		ImageIO.write(joinedImg, "jpg", os);
+		InputStream in = new ByteArrayInputStream(os.toByteArray());
+		
+    	return IOUtils.toByteArray(in);
     }
 	
+    public static BufferedImage joinBufferedImage(BufferedImage img1,
+    	      BufferedImage img2) {
+    	    int offset = 2;
+    	    int width = img1.getWidth() + img2.getWidth() + offset;
+    	    int height = Math.max(img1.getHeight(), img2.getHeight()) + offset;
+    	    BufferedImage newImage = new BufferedImage(width, height,
+    	        BufferedImage.TYPE_INT_ARGB);
+    	    Graphics2D g2 = newImage.createGraphics();
+    	    //Color oldColor = g2.getColor();
+    	    //g2.setPaint(Color.BLACK);
+    	    //g2.fillRect(0, 0, width, height);
+    	    //g2.setColor(oldColor);
+    	    g2.drawImage(img1, null, 0, 0);
+    	    g2.drawImage(img2, null, img1.getWidth() + offset, 0);
+    	    g2.dispose();
+    	    return newImage;
+    	  }
 
 }
