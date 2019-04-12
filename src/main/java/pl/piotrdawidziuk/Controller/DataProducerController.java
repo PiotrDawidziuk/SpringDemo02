08dccdde4cc7d82fef8e7da2cdb61c39a71ad194
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-04-12 14:06:46 +0200

    controller shows pictures and files

--- src/main/java/pl/piotrdawidziuk/Controller/DataProducerController.java
+++ src/main/java/pl/piotrdawidziuk/Controller/DataProducerController.java
@@ -1,13 +1,14 @@
 package pl.piotrdawidziuk.Controller;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
 import org.apache.commons.io.IOUtils;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.io.IOException;
-import java.io.InputStream;
 
 @Controller
 public class DataProducerController {
@@ -25,13 +26,16 @@ public class DataProducerController {
 
     @GetMapping(value = "/get-image-with-media-type", produces = MediaType.IMAGE_JPEG_VALUE)
     public @ResponseBody byte[] getImageWithMediaType() throws IOException {
-        final InputStream in = getClass().getResourceAsStream("https://vignette.wikia.nocookie.net/uncyclopedia/images/b/be/Cat.JPG");
+    	InputStream in = new URL("https://vignette.wikia.nocookie.net/uncyclopedia/images/b/be/Cat.JPG").openStream();  
+
+    	//final InputStream in = getClass().getResourceAsStream("https://vignette.wikia.nocookie.net/uncyclopedia/images/b/be/Cat.JPG");
         return IOUtils.toByteArray(in);
     }
 
     @GetMapping(value = "/get-file", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
     public @ResponseBody byte[] getFile() throws IOException {
-        final InputStream in = getClass().getResourceAsStream("https://www.w3.org/TR/PNG/iso_8859-1.txt");
+    	InputStream in = new URL("https://www.w3.org/TR/PNG/iso_8859-1.txt").openStream();  
+      //  final InputStream in = getClass().getResourceAsStream("/com/baeldung/produceimage/data.txt");
         return IOUtils.toByteArray(in);
     }
 
