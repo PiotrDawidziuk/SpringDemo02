6cdd14015480c1e90a79b703b3e45ebf4bfd3e19
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-04-15 08:43:30 +0200

    addcolor function and 4 picture version)

--- src/main/java/pl/piotrdawidziuk/Controller/JoinImageController.java
+++ src/main/java/pl/piotrdawidziuk/Controller/JoinImageController.java
@@ -23,6 +23,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class JoinImageController {
 	
+	  public static void setColorToGraphics(Graphics2D graph, int width, int height) {
+	    	Color oldColor = graph.getColor();
+	 	    graph.setPaint(Color.BLACK);
+	 	    graph.fillRect(0, 0, width, height);
+	 	    graph.setColor(oldColor);
+	    }
+	
 	@GetMapping(value = "/get-joined", produces = MediaType.IMAGE_JPEG_VALUE)
 	public @ResponseBody byte[] getJoinedImage() throws IOException {
 		
@@ -37,6 +44,8 @@ public class JoinImageController {
 		imgList.add(img1);
 		imgList.add(img2);
 		imgList.add(img3);
+		imgList.add(img3);
+
 		
 		BufferedImage joinedImg = joinBufferedImage(imgList);
 		
@@ -65,10 +74,14 @@ public class JoinImageController {
     	    newImage = new BufferedImage(width, height,
     	        BufferedImage.TYPE_INT_BGR);
     	    Graphics2D g2 = newImage.createGraphics();
-    	    Color oldColor = g2.getColor();
-    	    g2.setPaint(Color.BLACK);
-    	    g2.fillRect(0, 0, width, height);
-    	    g2.setColor(oldColor);
+    	    
+    	    setColorToGraphics(g2,width,height);
+    	    
+//    	    Color oldColor = g2.getColor();
+//    	    g2.setPaint(Color.BLACK);
+//    	    g2.fillRect(0, 0, width, height);
+//    	    g2.setColor(oldColor);
+//    	    
     	    g2.drawImage(img1, null, 0, 0);
     	    g2.drawImage(img2, null, img1.getWidth() + offset, 0);
     	    g2.dispose();
@@ -83,18 +96,47 @@ public class JoinImageController {
         	    newImage = new BufferedImage(width, height,
         	        BufferedImage.TYPE_INT_BGR);
         	    Graphics2D g2 = newImage.createGraphics();
-        	    Color oldColor = g2.getColor();
-        	    g2.setPaint(Color.BLACK);
-        	    g2.fillRect(0, 0, width, height);
-        	    g2.setColor(oldColor);
+        	    
+        	    setColorToGraphics(g2,width,height);
+//        	    Color oldColor = g2.getColor();
+//        	    g2.setPaint(Color.BLACK);
+//        	    g2.fillRect(0, 0, width, height);
+//        	    g2.setColor(oldColor);
+        	    
         	    g2.drawImage(img1, null, 0, 0);
         	    g2.drawImage(img2, null, img1.getWidth() + offset, 0);
         	    g2.drawImage(img3, null, img1.getWidth()+img2.getWidth() + offset, 0);
         	    g2.dispose();
+    	    } else if(img.size() == 4) {
+    	    	
+    	    	BufferedImage img1= img.get(0);
+        		BufferedImage img2= img.get(1);
+        		BufferedImage img3= img.get(2);
+        		BufferedImage img4 = img.get(3);
+        		
+    	    	int offset = 2;
+           	    
+    	    	int width = Collections.max(Arrays.asList ((img1.getWidth() + img2.getWidth()), (img3.getWidth() + img4.getWidth())) ) + offset;
+           	    
+           	    
+        	    int height = Collections.max(Arrays.asList ((img1.getHeight() + img3.getHeight()), (img2.getHeight() + img4.getHeight())) ) + offset;
+        	    newImage = new BufferedImage(width, height,
+        	        BufferedImage.TYPE_INT_BGR);
+        	    Graphics2D g2 = newImage.createGraphics();
+        	    
+        	    setColorToGraphics(g2,width,height);
+        	    g2.drawImage(img1, null, 0, 0);
+        	    g2.drawImage(img2, null, img1.getWidth() + offset, 0);
+        	    g2.drawImage(img3, null, 0, Collections.max(Arrays.asList(img1.getHeight(),img2.getHeight())) + offset);
+        	    g2.drawImage(img4, null, img1.getWidth() + offset, Collections.max(Arrays.asList(img1.getHeight(),img2.getHeight())) + offset);
+        	    g2.dispose();
+    	    	
     	    }
    
     	    
     	    return newImage;
     	  }
+    
+  
 
 }
