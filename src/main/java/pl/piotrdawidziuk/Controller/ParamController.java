0b35f6e99b99b83a646ae005fb21d0ab59829743
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-04-16 08:36:41 +0200

    get mapping test param

--- src/main/java/pl/piotrdawidziuk/Controller/ParamController.java
+++ src/main/java/pl/piotrdawidziuk/Controller/ParamController.java
@@ -1,17 +1,17 @@
 package pl.piotrdawidziuk.Controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ParamController {
 	
-	@PostMapping("/api")
+	@GetMapping("/api/foos")
 	@ResponseBody
-	public String addFoo(@RequestParam(name = "id") String fooId, @RequestParam String name) { 
-	    return "ID: " + fooId + " Name: " + name;
+	public String getFoos(@RequestParam String id) {
+	    return "ID: " + id;
 	}
 
 }
