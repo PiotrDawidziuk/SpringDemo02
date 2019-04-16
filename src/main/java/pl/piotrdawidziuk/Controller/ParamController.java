ee96d7955b26f15cda5f5f280c1d54ed9624c2b6
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-04-16 08:51:44 +0200

    post mapping try

--- src/main/java/pl/piotrdawidziuk/Controller/ParamController.java
+++ src/main/java/pl/piotrdawidziuk/Controller/ParamController.java
@@ -2,6 +2,7 @@ package pl.piotrdawidziuk.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -12,6 +13,12 @@ public class ParamController {
 	@ResponseBody
 	public String getFoos(@RequestParam String id) {
 	    return "ID: " + id;
+	}
+	
+	@PostMapping("/api/foos")
+	@ResponseBody
+	public String addFoo(@RequestParam(name = "id") String fooId, @RequestParam String name) { 
+	    return "ID: " + fooId + " Name: " + name;
 	}
 
 }
